b151e64b1ee0b10eaa86ba5f6e46778c652a6471
Basri Dogan <basri.dogan.71@gmail.com>
2019-11-06 15:03:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(input_names_array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map{}
+  planeteer_calls.map{|item|""}
 end
 
 def long_planeteer_calls# code an argument here
