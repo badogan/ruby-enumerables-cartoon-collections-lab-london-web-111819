8adb103e50924cf522ed86fae6ece9e663dfcba8
Basri Dogan <basri.dogan.71@gmail.com>
2019-11-06 15:47:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,8 @@ end
 def find_the_cheese(input_snacks_array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  input_snacks_array.include?
+  input_snacks_array.map{|item|cheese_types.include?(item)}
+  
   input_snacks_array.find do |item_s|
     cheese_types.find do |item_c|
       [item_s,item_c]
