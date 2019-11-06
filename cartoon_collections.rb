065d7504f753cc708c4853ef4ef165005482a80a
Basri Dogan <basri.dogan.71@gmail.com>
2019-11-06 15:34:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,9 +14,10 @@ def long_planeteer_calls(input_words_array)
   input_words_array.any?{|item|item.size >4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(input_snacks_array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
 
 array = ["Basri","Arda","Zeynep"]
