a4198263babcfef26113f32cc5870dd0273058a8
Basri Dogan <basri.dogan.71@gmail.com>
2019-11-06 15:26:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(input_words_array)
-  input_words_array.map{|item|item.length>4}
+  input_words_array.map{|item|item.any?(item.length>4)}
 end
 
 def find_the_cheese# code an argument here
